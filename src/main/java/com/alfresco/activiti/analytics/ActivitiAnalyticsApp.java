044e1311028ad3f2285316970ab0dcc582ef437d
Ciju <ciju.joseph@alfresco.com>
2018-05-09 23:13:35 -0700

    Removed commented code

--- src/main/java/com/alfresco/activiti/analytics/ActivitiAnalyticsApp.java
+++ src/main/java/com/alfresco/activiti/analytics/ActivitiAnalyticsApp.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-//@EntityScan( basePackages = {"com.activiti.domain"} )
-//@EnableJpaRepositories("com.activiti.repository")
 public class ActivitiAnalyticsApp {
 
 	public static void main(String[] args) {
